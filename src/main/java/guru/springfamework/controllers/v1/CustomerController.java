e9eb64610a2f20abdc832c624556e62f8aa47aea
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-26 16:34:37 +0300

    get and create customer

---
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -0,0 +1,44 @@
+package guru.springfamework.controllers.v1;
+
+import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.api.v1.model.CustomerListDTO;
+import guru.springfamework.services.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/api/v1/customers/")
+public class CustomerController {
+  private final CustomerService customerService;
+
+  public CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
+
+  @GetMapping
+  public ResponseEntity<CustomerListDTO> getListOfCustomers() {
+
+    return new ResponseEntity<>(
+        new CustomerListDTO(customerService.getAllCustomers()), HttpStatus.OK);
+  }
+
+  @GetMapping({"{id}"})
+  public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable Long id) {
+    return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
+    final CustomerDTO customerDto = customerService.createNewCustomer(customerDTO);
+    log.debug("customerDto: {}", customerDto);
+    return new ResponseEntity<>(customerDto, HttpStatus.CREATED);
+  }
+}
---
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -0,0 +1,55 @@
+package guru.springfamework.services;
+
+import guru.springfamework.api.v1.mapper.CustomerMapper;
+import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.domain.Customer;
+import guru.springfamework.repositories.CustomerRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+  private final CustomerRepository customerRepository;
+  private final CustomerMapper customerMapper;
+
+  public CustomerServiceImpl(CustomerRepository customerRepository, CustomerMapper customerMapper) {
+    this.customerRepository = customerRepository;
+    this.customerMapper = customerMapper;
+  }
+
+  @Override
+  public List<CustomerDTO> getAllCustomers() {
+    return customerRepository.findAll().stream()
+        .map(
+            customer -> {
+              CustomerDTO customerDto = customerMapper.customerToCustomerDTO(customer);
+              customerDto.setCustomerUrl("/api/v1/customer/" + customer.getId());
+              return customerDto;
+            })
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public CustomerDTO getCustomerById(Long id) {
+    return customerRepository
+        .findById(id)
+        .map(customerMapper::customerToCustomerDTO)
+        .orElseThrow(RuntimeException::new);
+  }
+
+  @Override
+  public CustomerDTO createNewCustomer(CustomerDTO customerDTO) {
+
+    Customer customer = customerMapper.customerDtoToCustomer(customerDTO);
+
+    Customer savedCustomer = customerRepository.save(customer);
+
+    CustomerDTO returnDto = customerMapper.customerToCustomerDTO(savedCustomer);
+
+    returnDto.setCustomerUrl("/api/v1/customer/" + savedCustomer.getId());
+
+    return returnDto;
+  }
+}
